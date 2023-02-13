819b0b2e68f88a6935b9eaa382911c8365dde979
qz-tel <94819843+qz-tel@users.noreply.github.com>
2023-02-13 17:10:34 +0800

    Add files via upload

--- web_zhaogu_advance/__manifest__.py
+++ web_zhaogu_advance/__manifest__.py
@@ -12,7 +12,6 @@
         "views/web_zhaogu_shipping_detail_view.xml",
         "views/web_my_portal_detail_view.xml",
         "views/web_my_portal_invoice_list_view.xml",
-        "views/web_my_portal_invoice_inherit_view.xml",
         "data/ir_config_parameter_token.xml",
     ],
     'qweb':[
