29206f9180d8bee9e55f02aed10871d730c1562f
qz-tel <94819843+qz-tel@users.noreply.github.com>
2023-02-15 15:39:44 +0800

    Add files via upload

--- shipping_bills/models/shipping_large_parcel.py
+++ shipping_bills/models/shipping_large_parcel.py
@@ -13,6 +13,7 @@ class ShippingLargeParcel(models.Model):
     logistics_provider = fields.Char('物流商', readonly=True)
     logistics_tracking_code = fields.Char('物流追踪码', readonly=True)
     customer_waybill_ids = fields.One2many('shipping.bill', 'large_parcel', string='客户运单', readonly=True)
+    site_id = fields.Many2one('res.partner', '站点')
 
 
 
--- shipping_bills/wizard/shipping_bill_update_transport_wizard.py
+++ shipping_bills/wizard/shipping_bill_update_transport_wizard.py
@@ -4,7 +4,8 @@ import logging
 
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
-from datetime import date
+from datetime import date, datetime
+
 
 class ShippingBillUpdateTransportWizard(models.TransientModel):
     _name = 'shipping.bill.update.transport.wizard'
@@ -13,7 +14,17 @@ class ShippingBillUpdateTransportWizard(models.TransientModel):
 
     def apply(self):
         _today = date.today()
-        for i,data in enumerate(self.data.split('\n')):
+        site = False
+        # 创建大包裹
+        large_parcel = self.env['shipping.large.parcel'].create({
+            'name': '123',
+            'waybill_number': '1234',
+            'logistics_provider': '12345',
+            'logistics_tracking_code': '123456',
+        })
+
+        dispatch_time = datetime.strptime(str(large_parcel.create_date), '%Y年%m月%d日 %H:%M:%S')
+        for i, data in enumerate(self.data.split('\n')):
             if not data:
                 continue
             _datas = data.split('\t')
@@ -22,12 +33,31 @@ class ShippingBillUpdateTransportWizard(models.TransientModel):
             _name, logistics, tracking_no = _datas
             _name, logistics, tracking_no = _name.strip(), logistics.strip(), tracking_no.strip()
             shipping_bill = self.env['shipping.bill'].search([
-                '|',('name','=',_name),('sale_fetch_no','=',_name),
-                ('state','=','valued')],limit=1)
+                '|', ('name', '=', _name), ('sale_fetch_no', '=', _name),
+                ('state', '=', 'valued')], limit=1)
+            if not site:
+                site = shipping_bill.sale_site_id.id
 
             shipping_bill.write({
                 'out_date': _today,
                 'logistics': logistics,
                 'tracking_no': tracking_no,
                 'state': 'transported',
+                'large_parcel': large_parcel.id
             })
+
+        large_parcel.site_id = site
+
+        # 发送邮件
+        name = large_parcel.name or ''
+        logistics = large_parcel.logistics_provider or ''
+        tracking_no = large_parcel.logistics_tracking_code or ''
+        mail = self.env['mail.mail'].create({
+            'subject': '包裹已发到你的站点。',
+            'email_from': 'info@sinefine.store',
+            'email_to': large_parcel.site_id.email,
+            'body_html': '<p>您站点的包裹已于' + dispatch_time + '发出：' + name + '</p>' + '<p>运单号：' + name + '</p>' + '<p>物流商：' + logistics + '</p>' + '<p>物流追踪码：' + tracking_no + '</p>'
+        })
+        mail.send()
+
+
