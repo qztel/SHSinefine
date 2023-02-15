c4f96d7c02e38c572d7b5b0ae0b24a5ed9dd6a11
qz-tel <94819843+qz-tel@users.noreply.github.com>
2023-02-15 16:06:23 +0800

    Add files via upload

--- shipping_bills/models/shipping_large_parcel.py
+++ shipping_bills/models/shipping_large_parcel.py
@@ -9,7 +9,6 @@ class ShippingLargeParcel(models.Model):
     _rec_name = 'name'
 
     name = fields.Char('包裹号')
-    waybill_number = fields.Char('运单号', readonly=True)
     logistics_provider = fields.Char('物流商', readonly=True)
     logistics_tracking_code = fields.Char('物流追踪码', readonly=True)
     customer_waybill_ids = fields.One2many('shipping.bill', 'large_parcel', string='客户运单', readonly=True)
