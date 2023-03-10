905564641ec361d63599af8805d01b9ad710c1e3
qz-tel <94819843+qz-tel@users.noreply.github.com>
2023-03-10 10:56:01 +0800

    发送消息跳转测试

---
+++ web_zhaogu_advance_test/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name':"OSCG-昆明兆古网站",
+    'description':"""
+        昆明兆古网站
+    """,
+    'category': 'Hidden',
+    'version':'1.0',
+    'depends':['base', 'web', 'website'],
+    'data':[
+        "data/ir_config_parameter_token.xml",
+    ],
+    'qweb':[
+
+    ],
+    'auto_install': False,
+    'license': 'OEEL-1',
+}
