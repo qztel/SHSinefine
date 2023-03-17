e5b24c8135e386564d3d2ce4ed84b3e7c6cb11d0
qz-tel <94819843+qz-tel@users.noreply.github.com>
2023-03-17 19:12:19 +0800

    Add files via upload

--- shipping_bills/models/shipping_bill.py
+++ shipping_bills/models/shipping_bill.py
@@ -15,30 +15,35 @@ class ShippingBill(models.Model):
     state = fields.Selection([('draft', '草稿'), ('paired', '已匹配'), ('valued', '已计费'),
                               ('returned', '已退运'), ('transported', '已转运'), ('arrived', '已到站点'),
                               ('signed', '已签收'), ('discarded', '丢弃')], default='draft', string='状态')
-    stage_id = fields.Many2one('shipping.state', string="阶段", compute="_compute_shipping_stage_id", store=True, readonly=False)
+    stage_id = fields.Many2one('shipping.state', string="阶段", ondelete='restrict', track_visibility='onchange', index=True)
 
-    @api.depends('state')
-    def _compute_shipping_stage_id(selfs):
+    def compute_shipping_stage_id(selfs):
         for self in selfs:
             if self.sale_order_id:
                 if self.state == 'valued' and self.sale_invoice_payment_state == '支付未完成':
-                    self.stage_id = 10
+                    self.stage_id = 2
                 elif self.state == 'valued' and self.sale_invoice_payment_state == '支付已完成':
-                    self.stage_id = 11
+                    self.stage_id = 3
                 elif self.state == 'transported':
-                    self.stage_id = 12
+                    self.stage_id = 4
                 elif self.state == 'arrived':
-                    self.stage_id = 13
-                elif self.state == 'returned':
-                    self.stage_id = 14
-                elif self.state == 'discarded':
-                    self.stage_id = 15
+                    self.stage_id = 5
                 elif self.state == 'signed':
-                    self.stage_id = 16
+                    self.stage_id = 6
+                elif self.state == 'returned':
+                    self.stage_id = 7
+                elif self.state == 'discarded':
+                    self.stage_id = 8
                 else:
                     self.stage_id = False
             else:
-                self.stage_id = 9
+                self.stage_id = 1
+
+    @api.onchange('state')
+    def onchange_state(selfs):
+        for self in selfs:
+            self.compute_shipping_stage_id()
+
 
     ref = fields.Char(string='参考号（每天）', copy=False)
 
