786a0d60c62e6fc803341a68e8413bedb7711b0b
qz-tel <94819843+qz-tel@users.noreply.github.com>
2023-03-17 20:45:41 +0800

    Update shipping_state.py

--- shipping_bills/models/shipping_state.py
+++ shipping_bills/models/shipping_state.py
@@ -4,6 +4,8 @@ from odoo import models, fields, api, _
 class ShippingState(models.Model):
     _name = 'shipping.state'
     _description = '阶段'
+    _rec_name = 'name'
+    _order = 'sequence'
 
     name = fields.Char('名称')
     fold = fields.Boolean('是否在看板中折叠')
