80335015ddf568c2071dc9c4d29db06f2e12725d
qz-tel <94819843+qz-tel@users.noreply.github.com>
2023-03-17 20:38:52 +0800

    运单看板视图修改

--- shipping_bills/models/shipping_state.py
+++ shipping_bills/models/shipping_state.py
@@ -6,5 +6,9 @@ class ShippingState(models.Model):
     _description = '阶段'
 
     name = fields.Char('名称')
-    fold = fields.Boolean('是否在看板中折叠？')
+    fold = fields.Boolean('是否在看板中折叠')
+    sequence = fields.Integer(
+        "Sequence", default=10,
+        help="Gives the sequence order when displaying a list of stages.")
+    requirements = fields.Text("Requirements")
 
