4067bf636009989027f0bc33efdc7f6e11df8923
qz-tel <94819843+qz-tel@users.noreply.github.com>
2023-03-23 17:39:17 +0800

    菜单修改

--- shipping_bills/models/shipping_large_parcel.py
+++ shipping_bills/models/shipping_large_parcel.py
@@ -17,7 +17,7 @@ class ShippingLargeParcel(models.Model):
     length = fields.Float('长(CM)')
     width = fields.Float('宽(CM)')
     height = fields.Float('高(CM)')
-    volume = fields.Float('体积(m³)', default=lambda self: self.length * self.width * self.height / 10 ^ 6)
+    volume = fields.Float('体积(m³)')
 
     shipping_bill_ids = fields.Many2many(
         'shipping.bill', 'shipping_bill_large_parcel_rel', 'large_parcel_id', 'shipping_bill_id', '客户运单',
