2852eddbf29d55fc28a0de5675626ab5bad61ae0
qz-tel <94819843+qz-tel@users.noreply.github.com>
2023-04-17 09:42:24 +0800

    国际短信发送及个人中心页面处理
    国际短信发送及个人中心页面处理

---
+++ zhaogu_website_my_home/controller/main.py
@@ -0,0 +1,25 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# Original Copyright 2015 Eezee-It, modified and maintained by Odoo.
+
+import logging
+
+from odoo import http
+from odoo.exceptions import UserError
+from odoo.http import request
+from werkzeug.urls import url_join, url_encode
+
+_logger = logging.getLogger(__name__)
+
+
+class Controller(http.Controller):
+
+    @http.route('/my/personal/home', type='http', auth='public', methods=['GET'], website=True)
+    def sale_fill_order_create_view(self):
+        partner = request.env.user.partner_id
+        values = {
+            'partner': partner
+        }
+        return request.render('zhaogu_website_my_home.haitao_website_my_home', values)
+
+
+
