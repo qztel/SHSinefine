ba105569eef50320b21dd8689e0110d59e0848ad
qz-tel <94819843+qz-tel@users.noreply.github.com>
2023-05-17 14:50:19 +0800

    包裹退运丢弃时显示可用取件码，更改退运单号时，退运日期赋值
    包裹退运丢弃时显示可用取件码，更改退运单号时，退运日期赋值

--- shipping_bills/models/shipping_bill.py
+++ shipping_bills/models/shipping_bill.py
@@ -106,6 +106,12 @@ class ShippingBill(models.Model):
     return_contact = fields.Char('退运收件人')
     return_mobile = fields.Char('退运联系电话')
     return_name = fields.Char('退运单号')
+
+    @api.onchange('return_name')
+    def _onchage_return_name(self):
+        _today = date.today()
+        self.returned_date =  _today,
+
     returned_date = fields.Date('退运日期')
     in_days = fields.Integer('入库天数')
 
