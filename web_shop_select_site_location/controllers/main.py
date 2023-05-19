363709217e481603467247181374689d09090720
qz-tel <94819843+qz-tel@users.noreply.github.com>
2023-05-19 17:34:21 +0800

    Add files via upload

--- web_shop_select_site_location/controllers/main.py
+++ web_shop_select_site_location/controllers/main.py
@@ -12,4 +12,17 @@ _logger = logging.getLogger(__name__)
 odoo_session = requests.Session()
 
 
-# class ZhaoguWeb(http.Controller):
+class WenShopSelectSiteLocation(http.Controller):
+
+    @http.route(['/select/delivery/type'], type='json', auth="user", website=True)
+    def website_select_delivery_type(self, type, site_id):
+        order = request.website.sale_get_order()
+        if site_id:
+            site_partner = request.env['crm.team'].browse(int(site_id)).site_id.id
+        else:
+            site_partner = order.partner_id.team_id.site_id
+        order.sudo().write({
+            'carrier_id': int(type),
+            'partner_team_site_id':site_partner
+        })
+        return {'state': '200'}
