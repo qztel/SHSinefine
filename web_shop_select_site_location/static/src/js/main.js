db682cb9cb5b2452d357b78bc1377e72bd6d760a
qz-tel <94819843+qz-tel@users.noreply.github.com>
2023-05-21 19:17:04 +0800

    Add files via upload

--- web_shop_select_site_location/static/src/js/main.js
+++ web_shop_select_site_location/static/src/js/main.js
@@ -1,33 +1,39 @@
 $(document).ready(function() {
-                  $('#select-site').select2()
-                  $('#select-site-table').css('display', 'none')
+  $('#select-site').select2()
+  $('#select-site-table').css('display', 'none')
 
-                  $(".o_delivery_carrier_select").click(function() {
-                    const way = $(this).children('label').text()
-                    const self = $(this)
+  const type_id = false
+  const site_id = false
 
-                    if (way == '站点自提'){
-                      $('#select-site-table').css('display', 'block')
-                    } else {
-                      $('#select-site-table').css('display', 'none')
-                      $('#select-site').val('0')
-                      $('.select2-selection__rendered').text('请选择')
-                      console.log($('#select-site').val())
-                    }
-                  })
+  $(".o_delivery_carrier_select").click(function() {
+    const way = $(this).children('label').text()
+    type_id = $(this).children('input').val()
+
+    delivery_type_select_ajax(type_id, site_id)
+
+    if (way == '站点自提'){
+      $('#select-site-table').css('display', 'block')
+    } else {
+      $('#select-site-table').css('display', 'none')
+      $('#select-site').val('0')
+      $('.select2-selection__rendered').text('请选择')
+      console.log($('#select-site').val())
+    }
+  })
+
+  $('#select-site').change(function() {
+    site_id = $(this).val()
+  })
 
 
-                  function delivery_type_select_ajax(self, val_id) {
-                    const type_id = self.children('input').val()
-                    const site_id = $('#select-site').val()
-
-                    url = '/select/delivery/type'
-                    let data = {
-                      type_id: type_id,
-                      site_id: site_id,
-                    }
-                    $.post(url,data,function(result){
-                       console.log(result)
-                    })
-                  }
-                })
+  function delivery_type_select_ajax(type_id, val_id) {
+    url = '/select/delivery/type'
+    let data = {
+      type_id: type_id,
+      site_id: site_id,
+    }
+    $.post(url,data,function(result){
+       console.log(result)
+    })
+  }
+})
