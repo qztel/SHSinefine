336593fd27322d58c4b1b5b8fd48cc1baeeb1a3a
qz-tel <94819843+qz-tel@users.noreply.github.com>
2023-05-25 12:10:37 +0800

    客户包裹预报选择其他分类，创建待定销售分类
    客户包裹预报选择其他分类，创建待定销售分类

--- zhaogu_sale/models/product_prdouct.py
+++ zhaogu_sale/models/product_prdouct.py
@@ -29,7 +29,7 @@ class ProductProduct(models.Model):
         for self in selfs:
             if self.sale_category_id and self.material_id:
                 if selfs.search_count([('sale_category_id','=',self.sale_category_id.id),
-                    ('material_id','=',self.material_id.id),('id','!=',self.id)]):
+                    ('material_id','=',self.material_id.id),('id','!=',self.id), ('active', '=', True)]):
                     raise UserError(f'产品中 {self.sale_category_id.name} + {self.material_id.name}'\
                         f' 已存在')
 
