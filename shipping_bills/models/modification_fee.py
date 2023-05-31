7528c04d23d11d3333ee17cdcb1f998ec404e088
qz-tel <94819843+qz-tel@users.noreply.github.com>
2023-05-31 10:37:49 +0800

    改泡费增加币种及汇率换算
    改泡费增加币种及汇率换算

--- shipping_bills/models/modification_fee.py
+++ shipping_bills/models/modification_fee.py
@@ -7,4 +7,9 @@ class ModificationFee(models.Model):
 
     name = fields.Char('名称')
     price = fields.Float('价格')
+    currency_id = fields.Many2one('res.currency', string="币种")
+
+    @api.onchange('currency_id')
+    def _onchange_currency_id(self):
+        self.price = self.price * self.currency_id.rate
 
